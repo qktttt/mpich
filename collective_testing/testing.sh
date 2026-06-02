f510042499cb1d319fdb1cb8d50854afb8c51335
Kunting Qi <kqi@uic.edu>
2026-06-02 10:15:10 -0500

    testing

--- collective_testing/testing.sh
+++ collective_testing/testing.sh
@@ -12,7 +12,7 @@ PROJECT_ROOT="/home/qik/projects/mpich"
 
 "$MPICH_PREFIX/bin/mpicxx" -std=c++11 -O2 $PROJECT_ROOT/$TEST_SRC -o $PROJECT_ROOT/$TEST_BIN
 
-"$MPICH_PREFIX/bin/mpiexec" -n 8 $PROJECT_ROOT/$TEST_BIN \
+"$MPICH_PREFIX/bin/mpiexec" -n 32 $PROJECT_ROOT/$TEST_BIN \
   --iters 5 \
   --output $PROJECT_ROOT/$CSV_OUT
 
